fbe414375d2a2676661476270167599618904ba2
Gabriel Omar Cotelli <g.cotelli@gmail.com>
2023-08-02 15:13:52 -0300

    Debugging

--- start-gemstone.sh
+++ start-gemstone.sh
@@ -31,9 +31,8 @@ echo "::endgroup::"
 echo "Mapping workspace to ${ROWAN_PROJECTS_HOME}/$INPUT_PROJECT_NAME"
 ln -s "${GITHUB_WORKSPACE}" "${ROWAN_PROJECTS_HOME}/$INPUT_PROJECT_NAME"
 
-ls -la "${ROWAN_PROJECTS_HOME}/$INPUT_PROJECT_NAME"
+ls "${ROWAN_PROJECTS_HOME}/$INPUT_PROJECT_NAME"
 ls -la "${GITHUB_WORKSPACE}"
-ls -la "${RUNNER_WORKSPACE}"
 
 echo "::group::Loading code"
 
